84bbfed732aee135eeeb43a55d7ef99ae1a76eee
ben davis <Benwdavis@yahoo.com>
2024-04-05 16:09:05 -0400

    Added account table and account model

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,6 +1,7 @@
 BEGIN TRANSACTION;
 
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS account;
 
 CREATE TABLE users (
 	user_id SERIAL,
@@ -8,6 +9,14 @@ CREATE TABLE users (
 	password_hash varchar(200) NOT NULL,
 	role varchar(50) NOT NULL,
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
+);
+
+CREATE TABLE account(
+username varchar(50) NOT NULL UNIQUE,
+	user_id int,
+	user_description varchar(500),
+	
+	FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
 COMMIT TRANSACTION;

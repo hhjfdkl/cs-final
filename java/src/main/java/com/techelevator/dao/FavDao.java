864c2d2065ce429383094e98877f1f0a63ff5ca6
Luke Watkins <lukewatkins2003@icloud.com>
2024-04-10 12:16:28 -0400

    add fav dao stuff

--- java/src/main/java/com/techelevator/dao/FavDao.java
+++ java/src/main/java/com/techelevator/dao/FavDao.java
@@ -2,7 +2,10 @@ package com.techelevator.dao;
 
 public interface FavDao {
 
-    public boolean addFav();
 
 
+
+    boolean addFav(int account_id, int movie_id);
+
+    boolean removeFav(int account_id, int movie_id);
 }

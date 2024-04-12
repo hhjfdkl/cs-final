8a69b767f1025af244dec96287c3c15fad59f15e
Nathaniel Koroman <nate.koroman@gmail.com>
2024-04-12 15:09:39 -0400

    Made beginning for reviews but not the controller

---
+++ java/src/main/java/com/techelevator/dao/ReviewDao.java
@@ -0,0 +1,20 @@
+package com.techelevator.dao;
+
+import com.techelevator.model.Review;
+
+import java.util.List;
+
+public interface ReviewDao
+{
+
+
+    List<Review> getReviewsByAccountId(int acctId);
+    List<Review> getReviewsByMovieId(int movieId);
+    List<Review> getAllReviews();   //admin
+
+    List<Review> getReviewsByRating(int rating);
+
+    Review getReviewByKey(int acctId, int movieId);
+
+
+}

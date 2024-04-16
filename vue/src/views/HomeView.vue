e41a82f9f3e9c9e1967106f520ef5ee97a646089
Luke Watkins <lukewatkins2003@icloud.com>
2024-04-16 16:23:32 -0400

    reverted some merge overriding

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,14 +1,14 @@
 <template>
-  <form @submit.prevent="searchMovies" class="search-change">
-    <input type="text" v-model="usersPerPage" placeholder="Movies per page" />
+  <H2>
+    Movies
+  </H2>
+  <!-- <form @submit.prevent="searchMovies" class="search-change">
+
+    <input class="mpp" type="number" v-model="usersPerPage" placeholder="Movies per page" />
     <button v-show="usersPerPage != ''" class="change" type="submit">Change</button>
-  </form>
+  </form> -->
   <div id="fav-upper">
     <ChangeOrder />
-  </div>
-  <div id="fav-bottom">
-    <button class="prev-next" id="prev" @click="previousPage">Previous Page</button>
-    <button class="prev-next" id="next" @click="nextPage">Next Page</button>
   </div>
   <div id="main-display">
     <MovieDetails v-for="movie in movies" v-bind:key="movie.movie_id" :movie="movie" />
@@ -19,19 +19,20 @@
   </div>
 </template>
 
-<script >
+<script>
 import MovieDetails from "../components/MovieDetails.vue";
 
 import MovieService from "../services/MovieService";
-
-
-
+import ChangeOrder from "../components/ChangeOrder.vue";
 
 export default {
   data() {
     return {
       movies: [],
-      usersPerPage: ""
+      usersPerPage: "",
+      selectedOrder: "",
+      selectedDirection: ""
+
     };
   }
 
@@ -48,10 +49,12 @@ export default {
   },
   components: {
     MovieDetails,
+    ChangeOrder
   },
 
   methods: {
     nextPage() {
+
       if (Number(this.$route.params.pageSize) > this.movies.length) return; //this doesn't work if the page was full
       this.$router.push({
         name: "movies",
@@ -59,16 +62,33 @@ export default {
           pageSize: this.$route.params.pageSize,
           page: Number(this.$route.params.page) + 1,
           sort: this.$route.params.sort
-        }
+        }, query: { asc: new URLSearchParams(window.location.search).get('asc') }
       });
 
       this.updateMovies();
     },
     updateMovies() {
-      console.log("updating")
-      MovieService.getMoviePage(this.$route.params.pageSize, this.$route.params.page, this.$route.params.sort).then((response) => {
-        this.movies = response.data;
-      });
+      let urlParams = new URLSearchParams(window.location.search);
+
+      let title = urlParams.get('title');
+
+
+      let ascOrDesc = urlParams.get('asc');
+      if (ascOrDesc == undefined) {
+        ascOrDesc = true;
+      }
+
+
+      if (title == null) {
+        MovieService.getMoviePageOrdered(this.$route.params.pageSize, this.$route.params.page, this.$route.params.sort, ascOrDesc).then((response) => {
+          this.movies = response.data;
+        });
+      } else {
+        MovieService.filterMoviesByTitle(this.$route.params.pageSize, this.$route.params.page, this.$route.params.sort, title).then((response) => {
+          this.movies = response.data;
+        });
+      }
+
     },
     previousPage() {
       if (Number(this.$route.params.page) <= 1) return;
@@ -78,8 +98,10 @@ export default {
           pageSize: this.$route.params.pageSize,
           page: Number(this.$route.params.page) - 1,
           sort: this.$route.params.sort
-        }
+        }, query: { asc: new URLSearchParams(window.location.search).get('asc') }
+
       });
+
     }
 
     ,
@@ -90,9 +112,13 @@ export default {
           pageSize: this.usersPerPage,
           page: 1,
           sort: this.$route.params.sort
-        }
+        },
+
       });
-    }
+    },
+
+
+
 
   }
 
@@ -109,9 +135,6 @@ h2 {
 
 #fav-upper {
   margin-left: 5%;
-  display: flex;
-  justify-content: space-between;
-  margin-bottom: 25px;
 }
 
 #main-display {
@@ -137,17 +160,33 @@ h2 {
   text-decoration-thickness: 2px
 }
 
+#fav-bottom {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 25px;
+}
+
 .prev-next {
   background-color: #fff0cb;
   color: #890304;
   border: 1px solid #890304;
   margin-top: 10px;
-
+  padding: 0.5rem 1rem;
+  border-radius: 4px;
+  cursor: pointer;
+  display: block;
+  margin: 0 auto;
+  font-family: 'league spartan';
 }
 
 #prev {
-  margin-left: 15%;
+  margin-left: 5%;
   margin-right: 1%;
+}
+
+#next {
+  margin-right: 5%;
+  margin-left: 1%;
 }
 
 .prev-next:hover {
@@ -172,5 +211,15 @@ h2 {
   margin: 10px;
   margin-left: 15%;
 
+}
+
+
+.mpp {
+  width: 100px;
+  padding: 0.5rem;
+  border-radius: 4px;
+  border: none;
+  box-shadow: 0 2px 4px #7B3911;
+  margin-right: 1rem;
 }
 </style>
